19ff9686636e97320528e95d26a90e4b82e0c984
thushanK <ktwimalaweera@gmail.com>
2021-05-19 18:49:24 +0530

    initial commit

---
+++ app/routes/room.route.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const roomController = require('./../controllers/room.controller');
+
+router.post('/add', roomController.add);
+
+router.get('/get', roomController.get);
+
+router.post('/update', roomController.update);
+
+router.delete('/delete/:id', roomController.delete);
+
+router.get('/getOne/:id', roomController.getOne);
+
+router.get('/getOneByName/:id', roomController.getOneByName);
+
+router.get('/health/check', roomController.healthCheck)
+
+module.exports = router
